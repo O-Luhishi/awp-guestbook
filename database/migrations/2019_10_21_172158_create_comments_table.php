c5c8f7bf89ef5481f91e99483a1128b608a12abb
Oluhishi <oeluhishi@gmail.com>
2019-10-21 19:26:20 +0100

    Created Code To Populate Database

---
+++ database/migrations/2019_10_21_172158_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name', 64)->nullable(false);
+            $table->text('comment')->nullable(false);
+            $table->integer('likes');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
